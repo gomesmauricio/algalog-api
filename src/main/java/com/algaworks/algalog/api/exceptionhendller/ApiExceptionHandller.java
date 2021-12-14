851a5fc95d845e2674457d1c674b9314b22ab553
gomesmauricio <josemauricio.gomes@gmail.com>
2021-12-14 15:45:22 -0300

    Criando endpoints para tabela cliente e iniciando tratamento de erros com exceptionHendller

---
+++ src/main/java/com/algaworks/algalog/api/exceptionhendller/ApiExceptionHandller.java
@@ -0,0 +1,9 @@
+package com.algaworks.algalog.api.exceptionhendller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandller extends ResponseEntityExceptionHandler {
+	
+}
