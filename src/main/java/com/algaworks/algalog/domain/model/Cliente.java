d64c4e8c878b1b3bdb038fe94caa0c37c7cdfbc5
gomesmauricio <josemauricio.gomes@gmail.com>
2021-12-15 07:19:44 -0300

    Tratando validação de campos com beanValidation

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,8 +25,21 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(min = 3, max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(min = 3, max = 60)
 	private String email;
+	
+	@NotBlank
+	@Email
+	@Size(max = 12)
+	@Column(name = "fone")
+	private String telefone;
 	
 	public void setId(Long id) {
 		this.id = id;
