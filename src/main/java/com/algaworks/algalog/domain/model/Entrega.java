818f04f365dd35a310931acc169d737667af4ab3
gomesmauricio <josemauricio.gomes@gmail.com>
2021-12-24 18:33:47 -0300

    Removendo validações da camada de domain - validações ja na camada de api

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -1,7 +1,6 @@
 package com.algaworks.algalog.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 import javax.persistence.Embedded;
@@ -12,10 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -30,18 +25,12 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Valid
-	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class) //converte de um group validation para outro
-	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
-	@Valid
-	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
-	@NotNull
 	private BigDecimal taxa;
 	
 	@Enumerated(EnumType.STRING)
